322fd564fa622c7d6b0eb0fc3e05d564e4b9f437
BubbaJoe <joe@Joes-Mac-mini.local>
2023-04-04 11:32:45 +0900

    add google tag manager

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         NEXT_PUBLIC_BASE_URL: 'https://bubbajoe.dev',
+        NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID: 'G-0FR317TLNJ',
     },
     images: {
         remotePatterns: [
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '@/styles/global.css'
 import Head from 'next/head'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
+  const measurementId = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID;
   return <>
     <div className='container'>
       <Head>
@@ -9,6 +11,14 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+            function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${measurementId}");
+            `,
+          }}
+        />
       </Head>
       <main><Component {...pageProps} /></main>
       <footer>
